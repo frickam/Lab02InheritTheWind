5f8cbfd06b9a8ea016791d9c351298c2821e02b9
frickam <frickam@mail.uc.edu>
2025-04-02 20:49:00 -0400

    Initial Commit

---
+++ Test/SalaryWorkerTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryWorkerTest {
+
+    @Test
+    void getAnnualSalary() {
+    }
+
+    @Test
+    void setAnnualSalary() {
+    }
+
+    @Test
+    void calculateWeeklyPay() {
+    }
+
+    @Test
+    void displayWeeklyPay() {
+    }
+
+    @Test
+    void toCSV() {
+    }
+
+    @Test
+    void toJSON() {
+    }
+
+    @Test
+    void toXML() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
